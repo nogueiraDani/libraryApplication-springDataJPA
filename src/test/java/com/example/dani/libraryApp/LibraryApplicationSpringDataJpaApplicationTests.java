fd6cac63c92195e2ae477e63342eee12305f314f
Daniela Nogueira Rampim <daninogueira.dev@gmail.com>
2024-12-25 22:30:16 -0300

    :tada: init: Initial commit

---
+++ src/test/java/com/example/dani/libraryApp/LibraryApplicationSpringDataJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dani.libraryApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApplicationSpringDataJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
