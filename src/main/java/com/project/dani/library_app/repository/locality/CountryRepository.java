7e4409be8880ec1e1ee668ae9300f12dee274f8e
Daniela Nogueira Rampim <daninogueira.dev@gmail.com>
2024-12-30 22:14:21 -0300

    🎇feat: add queries to crud

--- src/main/java/com/project/dani/library_app/repository/locality/CountryRepository.java
+++ src/main/java/com/project/dani/library_app/repository/locality/CountryRepository.java
@@ -1,8 +1,21 @@
 package com.project.dani.library_app.repository.locality;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import com.project.dani.library_app.entity.locality.Country;
 
 public interface CountryRepository extends JpaRepository<Country, Long> {
+
+    @Modifying
+    @Query("UPDATE Country c SET c.name = :name WHERE c.id = :id")
+    void updateById(Long id, String name);
+
+    @Query("SELECT c FROM Country c ORDER BY c.name ASC")
+    List<Country> findAllOrderByNameAsc();
+
+    @Query("SELECT c FROM Country c ORDER BY c.name DESC")
+    List<Country> findAllOrderByNameDesc();
 
 }
