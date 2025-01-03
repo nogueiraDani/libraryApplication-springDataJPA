753089c64719f6f5f074baab234d2398ec3c4ef2
Daniela Nogueira Rampim <daninogueira.dev@gmail.com>
2025-01-02 22:13:34 -0300

    ♻️ organizing code

--- src/main/java/com/project/dani/library_app/controller/locality/StateController.java
+++ src/main/java/com/project/dani/library_app/controller/locality/StateController.java
@@ -3,17 +3,16 @@ package com.project.dani.library_app.controller.locality;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import com.project.dani.library_app.entity.locality.Country;
-import com.project.dani.library_app.entity.locality.State;
-import com.project.dani.library_app.service.locality.StateService;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.project.dani.library_app.entity.locality.State;
+import com.project.dani.library_app.service.locality.StateService;
 
 
 @RestController
